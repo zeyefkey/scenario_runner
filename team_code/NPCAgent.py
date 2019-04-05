50ad8ad2f6ae171c03c259730edc8da85802c8a6
German Ros <germanros1987@gmail.com>
2019-04-05 16:09:52 -0700

    NPCAgent for testing

--- team_code/NPCAgent.py
+++ team_code/NPCAgent.py
@@ -3,7 +3,7 @@ import scipy.misc
 import carla
 from agents.navigation.basic_agent import *
 
-from srunner.scenariomanager.carla_data_provider import CarlaActorPool
+from srunner.scenariomanager.carla_data_provider import CarlaDataProvider
 
 from srunner.challenge.autoagents.autonomous_agent import AutonomousAgent, Track
 
@@ -11,17 +11,7 @@ class NPCAgent(AutonomousAgent):
     def setup(self, path_to_conf_file):
         self.track = Track.ALL_SENSORS_HDMAP_WAYPOINTS
         self.route_assigned = False
-
-        hero_actor = None
-        list_actors = list(CarlaActorPool.get_actors())
-        for _, actor in list_actors:
-            if actor.attributes['role_name'] == 'hero':
-                hero_actor = actor
-                break
-
-        if hero_actor:
-            self._agent = BasicAgent(hero_actor)
-
+        self._agent = None
 
     def sensors(self):
         """
@@ -57,23 +47,36 @@ class NPCAgent(AutonomousAgent):
         return sensors
 
     def run_step(self, input_data):
+        control = carla.VehicleControl()
+        control.steer = 0.0
+        control.throttle = 0.0
+        control.brake = 0.0
+        control.hand_brake = False
+
+        if not self._agent:
+            hero_actor = None
+            for actor in CarlaDataProvider.get_world().get_actors():
+                if 'role_name' in actor.attributes and actor.attributes['role_name'] == 'hero':
+                    hero_actor = actor
+                    break
+            if hero_actor:
+                self._agent = BasicAgent(hero_actor)
+
+            return control
+
         if not self.route_assigned:
             if self._global_plan:
-                import pdb; pdb.set_trace()
-                self._agent._local_planner.set_global_plan(self._global_plan)
+                plan = []
+
+                for transform, road_option in self._global_plan_world_coord:
+                    wp = CarlaDataProvider.get_map().get_waypoint(transform.location)
+                    plan.append((wp, road_option))
+
+                self._agent._local_planner.set_global_plan(plan)
                 self.route_assigned = True
 
-            control = carla.VehicleControl()
-            control.steer = 0.0
-            control.throttle = 0.0
-            control.brake = 0.0
-            control.hand_brake = False
-
         else:
-            control = carla.VehicleControl()
-            control.steer = 0.0
-            control.throttle = 0.0
-            control.brake = 0.0
-            control.hand_brake = False
+            print("--- Moving!")
+            control = self._agent.run_step()
 
         return control
