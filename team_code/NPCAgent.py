f1ab2f937638a0d93e458717e182b83f1ad17dad
German Ros <germanros1987@gmail.com>
2019-04-05 15:32:11 -0700

    undo me

--- team_code/NPCAgent.py
+++ team_code/NPCAgent.py
@@ -3,12 +3,24 @@ import scipy.misc
 import carla
 from agents.navigation.basic_agent import *
 
+from srunner.scenariomanager.carla_data_provider import CarlaActorPool
+
 from srunner.challenge.autoagents.autonomous_agent import AutonomousAgent, Track
 
 class NPCAgent(AutonomousAgent):
     def setup(self, path_to_conf_file):
         self.track = Track.ALL_SENSORS_HDMAP_WAYPOINTS
-        self._agent = BasicAgent(actor)
+        self.route_assigned = False
+
+        hero_actor = None
+        list_actors = list(CarlaActorPool.get_actors())
+        for _, actor in list_actors:
+            if actor.attributes['role_name'] == 'hero':
+                hero_actor = actor
+                break
+
+        if hero_actor:
+            self._agent = BasicAgent(hero_actor)
 
 
     def sensors(self):
@@ -45,7 +57,23 @@ class NPCAgent(AutonomousAgent):
         return sensors
 
     def run_step(self, input_data):
+        if not self.route_assigned:
+            if self._global_plan:
+                import pdb; pdb.set_trace()
+                self._agent._local_planner.set_global_plan(self._global_plan)
+                self.route_assigned = True
 
+            control = carla.VehicleControl()
+            control.steer = 0.0
+            control.throttle = 0.0
+            control.brake = 0.0
+            control.hand_brake = False
 
+        else:
+            control = carla.VehicleControl()
+            control.steer = 0.0
+            control.throttle = 0.0
+            control.brake = 0.0
+            control.hand_brake = False
 
         return control
