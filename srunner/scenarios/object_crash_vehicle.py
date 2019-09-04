dc9f99d133c4104dff4cb2dabdc0d82f303e5ebe
Felipe Codevilla <felipe.alcm@gmail.com>
2019-09-04 19:08:19 -0400

    add physics

--- srunner/scenarios/object_crash_vehicle.py
+++ srunner/scenarios/object_crash_vehicle.py
@@ -193,7 +193,8 @@ class DynamicObjectCrossing(BasicScenario):
         if self._adversary_type is False:
             self._walker_yaw = orientation_yaw
             self._other_actor_target_velocity = 3 + (0.4 * self._num_lane_changes)
-            walker = CarlaActorPool.request_new_actor('walker.*', transform)
+            walker = CarlaActorPool.request_new_actor('walker*', transform)
+
             adversary = walker
         else:
             self._other_actor_target_velocity = self._other_actor_target_velocity * self._num_lane_changes
