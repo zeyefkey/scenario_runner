7cbd539ee8d95dde18f0eac7127a212e46b40c93
Felipe Codevilla <felipe.alcm@gmail.com>
2019-09-16 16:59:55 -0400

    8 is the distance

--- srunner/scenarios/object_crash_vehicle.py
+++ srunner/scenarios/object_crash_vehicle.py
@@ -301,7 +301,7 @@ class DynamicObjectCrossing(BasicScenario):
             start_condition = InTriggerDistanceToLocationAlongRoute(self.ego_vehicle,
                                                                     self._ego_route,
                                                                     self.transform.location,
-                                                                    11)
+                                                                    12)
         else:
             start_condition = InTimeToArrivalToVehicle(self.other_actors[0],
                                                        self.ego_vehicle,
